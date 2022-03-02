d1cfd1ecc107e6bffe084b6315680caf821e7af6
azikreed <azikreed@gmail.com>
2022-03-02 16:36:41 +0500

    movie

---
+++ movie_app/script.js
@@ -0,0 +1,63 @@
+const API_KEY = "api_key=14de105e69b49ce456130944a2806596";
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&"+API_KEY;
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const searchURL = BASE_URL + "/search/movie?"+API_KEY;
+
+getMovies(API_URL)
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+function getMovies(url) {
+    fetch(url).then(response => response.json()).then(data => {
+        console.log(data.results);
+        showMovies(data.results)
+    })
+}
+
+function showMovies(data){
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img src="${IMG_URL + poster_path}" alt="Image">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+            <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+
+        main.appendChild(movieEl)
+    });
+}
+
+function getColor(vote) {
+    if( vote >= 8){
+        return 'green';
+    } else if(vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm){
+        getMovies(searchURL+'&query='+searchTerm)
+    } else {
+        getMovies(API_URL)
+    }
+})
